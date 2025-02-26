9a9e233e84ab5e68d7ec0889b1b3b8b1760493b9
Артем Cмирнов <daft.funk91@gmail.com>
2025-02-26 13:47:17 +0300

    Дополнительное задание со звездочкой

---
+++ Task2_5_Star/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2_5_Star
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину трубы в метрах: ");
+            string input = Console.ReadLine();
+            double length = Convert.ToDouble(input);
+
+            double roundedLength = Math.Round(length * 2, MidpointRounding.AwayFromZero) / 2;
+
+            Console.WriteLine($"Округленная длина: {roundedLength}");
+        }
+    }
+}
+ 

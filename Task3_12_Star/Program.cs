e5eb2073d8d4e67055314b3594b563f52e217229
Артем Cмирнов <daft.funk91@gmail.com>
2025-03-04 11:54:35 +0300

    Создание заданий для домашней работы по 3 лекции

---
+++ Task3_12_Star/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3_12_Star
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
